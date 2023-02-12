5abe8721745740dd0e85271dd78701c5ea41f6da
Ivan194884 <ivangogol1948@gmail.com>
2023-02-12 10:21:33 +0200

    i

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');   
+        break;
+      }
+    }
+  }
+}
